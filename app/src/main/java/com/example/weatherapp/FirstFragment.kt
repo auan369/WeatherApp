f0ffb6cae5357678c96f6bffb489870bf3aa2414
Kum Yew <lkyew@live.com.sg>
2023-05-13 12:22:58 +0800

    Adding API Interface

--- app/src/main/java/com/example/weatherapp/FirstFragment.kt
+++ app/src/main/java/com/example/weatherapp/FirstFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.weatherapp.databinding.FragmentFirstBinding
 
@@ -14,6 +17,13 @@ import com.example.weatherapp.databinding.FragmentFirstBinding
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
+    private fun countMe(view: View) {
+        val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+        val countString = showCountTextView.text.toString()
+        var count = countString.toInt()
+        count++
+        showCountTextView.text = count.toString()
+    }
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -32,9 +42,26 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
+        binding.randomButton.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+        view.findViewById<Button>(R.id.toast_button).setOnClickListener {
+            // create a Toast with some text, to appear for a short time
+            val myToast = Toast.makeText(context, "Hello Toast!", Toast.LENGTH_SHORT)
+            // show the Toast
+            myToast.show()
+        }
+
+        val countString = "0"
+        var count2 = countString.toInt()
+
+        view.findViewById<Button>(R.id.count_button).setOnClickListener {
+            countMe(view)
+            count2 ++
+            val myCount = Toast.makeText(context, count2.toString(), Toast.LENGTH_SHORT)
+            myCount.show()
+        }
+
     }
 
     override fun onDestroyView() {
